9a6d722f8eedd5998ff9106c7e653d381a5e02d4
rosejtec <rosej.tec@gmail.com>
2020-07-26 22:26:56 +0530

    Updates

---
+++ precache-manifest.5bc3ff685293102fcdd71e83144e4fd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5f800afa83595a52cf650375185378",
+    "url": "/itigen_frontend/index.html"
+  },
+  {
+    "revision": "7c9290cb003aee112057",
+    "url": "/itigen_frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c05357e341f93f9d3ee",
+    "url": "/itigen_frontend/static/js/2.4c732875.chunk.js"
+  },
+  {
+    "revision": "add385feea935fb0b87f6246656b6eb4",
+    "url": "/itigen_frontend/static/js/2.4c732875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9290cb003aee112057",
+    "url": "/itigen_frontend/static/js/main.29f755d2.chunk.js"
+  },
+  {
+    "revision": "cb2ced3676afb6e2d276",
+    "url": "/itigen_frontend/static/js/runtime-main.3e15ca4b.js"
+  },
+  {
+    "revision": "80e5b5653ab974e1c9c33754dfb7bcd9",
+    "url": "/itigen_frontend/static/media/Midnight.80e5b565.jpg"
+  },
+  {
+    "revision": "a16d1dafb179ef1121fae2c08f372350",
+    "url": "/itigen_frontend/static/media/circle-cropped.a16d1daf.png"
+  }
+]);
